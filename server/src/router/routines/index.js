49dd7bec9873c2c8bbf2dbcc5b0d6ee6b6dcbd0f
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-05 18:18:45 -0200

    [ADD] added routine put endpoint

--- server/src/router/routines/index.js
+++ server/src/router/routines/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import get from './get';
 import post from './post';
 import del from './del';
+import put from './put';
 
 const router = express.Router();
 
 router.get('/', get);
 router.post('/', post);
 router.delete('/:routineId', del);
+router.put('/:routineId', put);
 
 export default router;
