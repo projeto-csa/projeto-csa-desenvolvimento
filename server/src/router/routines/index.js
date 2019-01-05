4ab98a2164f5403501d8e11858db0b345eb48d8f
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-05 17:35:53 -0200

    [ADD] added routine post endpoint

--- server/src/router/routines/index.js
+++ server/src/router/routines/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import get from './get';
+import post from './post';
 
 const router = express.Router();
 
 router.get('/', get);
+router.post('/', post);
 
 export default router;
