e1dbc03523e45ee7ea23b6009ad99bc2ba33a76d
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-05 19:58:16 -0200

    [ADD] Added routine get by id endpoint

--- server/src/router/routines/index.js
+++ server/src/router/routines/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import get from './get';
+import getById from './get_by_id';
 import post from './post';
 import del from './del';
 import put from './put';
@@ -8,6 +9,7 @@ import put from './put';
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:routineId', getById);
 router.post('/', post);
 router.delete('/:routineId', del);
 router.put('/:routineId', put);
