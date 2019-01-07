71b98091eccda667c75dcc6b5a844bb54df8ed1b
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-07 19:29:38 -0200

    added variations get endpoint

---
+++ server/src/router/variations/get.js
@@ -0,0 +1,18 @@
+import Variation from '../../models/variation';
+
+export default (req, res) => {
+  Variation.find((err, variations) => {
+    if (err) {
+      return res.status(500).json({ success: false, error: err });
+    }
+    if (variations.length > 0) {
+      return res.status(200).json({
+        success: true,
+        records: variations.length,
+        data: variations,
+      });
+    }
+    return res.status(200).json({ success: true, data: null });
+  })
+  .populate('routine', 'name');
+};
