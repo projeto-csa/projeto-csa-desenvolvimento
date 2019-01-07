0b77833c356ab46259f8c79f47409870032cab7c
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-07 19:40:37 -0200

    [ADD] Added variations routes to api

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 
 import comments from './comments';
 import routines from './routines';
+import variations from './variations';
+
 
 const router = express.Router();
 
@@ -14,5 +16,6 @@ router.get('/', (req, res) => {
 
 router.use('/comments', comments);
 router.use('/routines', routines);
+router.use('/variations', variations);
 
 export default router;
