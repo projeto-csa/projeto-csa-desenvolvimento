0a576cfc9f3ffdaf002ab48b72af0cda962f91c0
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-07 18:58:34 -0200

    [UPDATE] Updating variation properties

--- server/src/models/variation.js
+++ server/src/models/variation.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const variationSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
-  routine: {type : mongoose.Schema.Types.ObjectId, ref : 'Routine'},
+  routine: {type : mongoose.Schema.Types.ObjectId, ref : 'Routine', required: true},
   description: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Variation', variationSchema);
