19f61038c520eb726d75d7b6b871e8995d8cdb38
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-14 21:51:18 -0200

    added variation get by id endpoint

--- server/src/router/routines/get_by_id.js
+++ server/src/router/routines/get_by_id.js
@@ -16,5 +16,6 @@ export default (req, res) => {
       });
     }
     return res.status(404).json({ success: true, error: 'Routine not Found' });
-  });
+  })
+  .populate('routine', '_id');
 };
--- server/src/router/variations/index.js
+++ server/src/router/variations/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import get from './get';
 import post from './post';
+import getById from './get_by_id';
 
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:variationId', getById);
 router.post('/', post);
 
 export default router;
