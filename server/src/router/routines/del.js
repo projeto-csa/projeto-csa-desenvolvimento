5540ff2f8682062f861666491fad3466b9e9c1c3
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-14 22:41:17 -0200

    [UPDATE] updated delete endpoints so related documents can be deleted

--- server/src/router/routines/del.js
+++ server/src/router/routines/del.js
@@ -1,10 +1,12 @@
 import Routine from '../../models/routine';
+import Variation from '../../models/variation';
 
 export default (req, res) => {
   const { routineId } = req.params;
   if (!routineId) {
     return res.json({ success: false, error: 'No routine id provided' });
   }
+  Variation.deleteMany({ routine: routineId }, function (err) {});
   Routine.deleteOne({ _id: routineId }, (error, routine) => {
     if (error) return res.status(500).json({ success: false, error });
     return res.status(200).json({ success: true, message: 'Routine deleted' });
