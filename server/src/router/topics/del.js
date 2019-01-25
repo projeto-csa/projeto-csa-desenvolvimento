14c2aa2cbf9311e762035f2920a268f15da458f7
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2019-01-25 17:25:33 -0200

    [FIX] Fixed delete endpoints

--- server/src/router/topics/del.js
+++ server/src/router/topics/del.js
@@ -6,7 +6,10 @@ export default (req, res) => {
     return res.json({ success: false, error: 'No topic id provided' });
   }
   Topic.deleteOne({ _id: topicId }, (error, topic) => {
-    if (error) return res.status(500).json({ success: false, error });
-    return res.status(200).json({ success: true, message: 'topic deleted' });
+    if (error) {
+      return res.status(500).json({ success: false, error });
+    }
+    if (topic) return res.status(200).json({ success: true, message: 'topic deleted' });
+    return res.status(400).json({ success: false, message: 'topic not found' });
   });
 };
